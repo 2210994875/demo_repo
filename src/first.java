554a6d7513d0beefc86e74e170746fb2ea348e70
Khushi Vohra <129832038+2210994875@users.noreply.github.com>
2025-07-02 12:53:57 +0530

    first.java

---
+++ src/first.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class first{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("What is your name");
+String name = sc.nextLine();
+System.out.println("Provide your roll number");
+long rollnumber = sc.nextLong();
+System.out.print("NAME :" + name+ " ROLLNUMBER :" + rollnumber);
+}
+}
